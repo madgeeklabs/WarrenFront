c6aaabd26309334cf5d2c201eea28f0e32922386
Alex Guerrero <alexguerrero1092@gmail.com>
2015-11-07 06:33:48 +0100

    Enable data sync with AJAx

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,52 +7,93 @@ const Drawer = require('../components/Drawer');
 const Section = require('../components/Section');
 const Text = require('../components/Text');
 const LineChart = require('react-chartjs').Line;
+const $ = require('jquery');
+
+var lineChartOptions = {
+    data: {
+        labels: [
+            '',
+            '10',
+            '11',
+            '12',
+            '13',
+            '14',
+            '15'
+        ],
+        datasets: [
+            {
+                label: 'My First dataset',
+                fillColor: 'rgba(220,220,220,0.2)',
+                strokeColor: 'rgba(220,220,220,1)',
+                pointColor: 'rgba(220,220,220,1)',
+                pointStrokeColor: '#fff',
+                pointHighlightFill: '#fff',
+                pointHighlightStroke: 'rgba(220,220,220,1)',
+                data: [
+                    5.45,
+                    3.25,
+                    6.20,
+                    2.75,
+                    3.10,
+                    3.25,
+                    4.75
+                ]
+            }
+        ]
+    },
+    options: {
+        bezierCurve: false
+    }
+};
 
 const Home = React.createClass({
     getInitialState() {
         return {
-            savings: 4.25,
-            lineChart: {
-                data: {
-                    labels: [
-                        '',
-                        '10',
-                        '11',
-                        '12',
-                        '13',
-                        '14',
-                        '15'
-                    ],
-                    datasets: [
-                        {
-                            label: 'My First dataset',
-                            fillColor: 'rgba(220,220,220,0.2)',
-                            strokeColor: 'rgba(220,220,220,1)',
-                            pointColor: 'rgba(220,220,220,1)',
-                            pointStrokeColor: '#fff',
-                            pointHighlightFill: '#fff',
-                            pointHighlightStroke: 'rgba(220,220,220,1)',
-                            data: [
-                                5.45,
-                                3.25,
-                                6.20,
-                                2.75,
-                                3.10,
-                                3.25,
-                                4.75
-                            ]
-                        }
-                    ]
-                },
-                options: {
-                    bezierCurve : false
-                }
-            }
+            value: 0,
+            coeficient: 1,
+            previousCoeficient: 1
         };
+    },
+    _fetchFromServer() {
+        var self = this;
+        $.ajax({
+            url: 'http://warren.ngrok.io/stock',
+            cache: false,
+            success: function(data) {
+                console.log('orders retrieved from server this is the first one:');
+                console.log(data);
+
+                var previousCoeficient =
+                    self.state.coeficient === data.coeficient ?
+                    self.state.previousCoeficient :
+                    self.state.coeficient;
+
+                self.setState({
+                    coeficient: data.coeficient,
+                    value: data.value,
+                    previousCoeficient: previousCoeficient
+                });
+
+                lineChartOptions.data.datasets[0].data.shift();
+                lineChartOptions.data.dataSets[0].data.push((this.state.value * this.state.coeficient).toFixed(2).parseFloat());
+
+                LineChart.updatePoints();
+            },
+            error: function(xhr, status, err) {
+                console.error(self.props.url, status, err.toString());
+            }
+        });
+    },
+
+    componentDidMount() {
+        this._fetchFromServer()
+        setInterval(this._fetchFromServer, 10000);
     },
 
     render() {
         var self = this;
+        var difference = this.state.value * this.state.coeficient - this.state.previousCoeficient * this.state.value;
+        var symbol = difference > 0 ? '+' : '-';
         return (
             <Container type='main'>
                 <Header onHeaderButtonClick={function() {
@@ -60,11 +101,12 @@ const Home = React.createClass({
                 }}/>
                 <Drawer ref='drawer' hideable/>
                 <Container type="content">
-                    <Text type='h2'>{this.state.savings.toFixed(2)}</Text>
-                    <Text type='h3Red'>{'+ 2,50 (5,90€)'}</Text>
+                    <Text type='h2'>{(this.state.value * this.state.coeficient).toFixed(2)}</Text>
+                    <Text type='h3Red'>{`${symbol} ${Math.abs(difference).toFixed(2)} (${this.state.coeficient} %)`}</Text>
                     <LineChart
-                        data={this.state.lineChart.data}
-                        options={this.state.lineChart.options}
+                        ref='chart'
+                        data={lineChartOptions.data}
+                        options={lineChartOptions.options}
                     />
                 </Container>
             </Container>
